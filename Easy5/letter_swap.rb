e945c328a675be8d0a0ed76aa957020d5f7ed6bc
nacocode <naco120692@gmail.com>
2021-11-02 22:43:58 -0400

    modified the code.

--- Easy5/letter_swap.rb
+++ Easy5/letter_swap.rb
@@ -5,19 +5,18 @@
 # swap('Abcde') == 'ebcdA'
 # swap('a') == 'a'
 
+def swap_first_last_cheracters(word)
+  word[0], word[-1] = word[-1], word[0]
+  word
+end
+
 def swap(string)
-  array = string.split
-  array = array.map {|word| switch_letters(word)}
+  array = string.split.map do |word| 
+    swap_first_last_cheracters(word)
+  end
   array.join(" ")
 end
 
-def switch_letters(word)
-  sub_array = word.chars
-  sub_array << sub_array[0]
-  sub_array[0] = sub_array[-2]
-  sub_array.delete_at(-2)
-  sub_array.join
-end
 
 p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
 p swap('Abcde') == 'ebcdA'
