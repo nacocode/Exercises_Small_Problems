7afd104ae754fda7be0539701e7461554cc6e677
nacocode <naco120692@gmail.com>
2021-11-03 16:56:29 -0400

    My first attempt all by myself.

---
+++ Easy5/letter_counter.rb
@@ -0,0 +1,30 @@
+# Write a method that takes a string with 
+# one or more space separated words and 
+# returns a hash that shows the number of words of different sizes.
+# 
+# word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+# word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+# word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+# word_sizes('') == {}
+
+def word_sizes(string)
+  hash = {}
+  string.split.each do |word|
+  hash[word.size] ||= 0
+  hash[word.size] += 1 if hash[word.size]
+  end
+
+  hash.sort.to_h
+end
+
+p word_sizes('Four score and seven.')
+p word_sizes('Hey diddle diddle, the cat and the fiddle!')
+p word_sizes("What's up doc?")
+p word_sizes('')
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+p word_sizes('') == {}
+
+
+
