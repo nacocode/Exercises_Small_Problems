1348f8041dde6780929005809e176d6b66d0fbae
nacocode <naco120692@gmail.com>
2021-11-04 11:18:49 -0400

    Removed the remove_symbol method.

--- Easy5/letter_counter2.rb
+++ Easy5/letter_counter2.rb
@@ -1,21 +1,14 @@
 def word_sizes(string)
   counts = Hash.new(0)
-  string = remove_symbol(string)
   string.split.each do |word|
-    counts[word.size] += 1
+    clean_word = word.gsub(/[\W]/, "")
+    counts[clean_word.size] += 1
   end
   counts.sort.to_h
 end
-
-def remove_symbol(string)
-  string = string.split.map do |word|
-    word.gsub(/[\W]/, "")
-  end
-  string.join(" ")
-end
-
 
 p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
 p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
 p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
 p word_sizes('') == {}
+
