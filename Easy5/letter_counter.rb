57aef51de65a3185b89ea9297c1a2002a9d1eaf6
nacocode <naco120692@gmail.com>
2021-11-03 22:01:47 -0400

    Compare with another solution

--- Easy5/letter_counter.rb
+++ Easy5/letter_counter.rb
@@ -8,23 +8,30 @@
 # word_sizes('') == {}
 
 def word_sizes(string)
-  hash = {}
+  hash = Hash.new
   string.split.each do |word|
   hash[word.size] ||= 0
   hash[word.size] += 1 if hash[word.size]
   end
-
   hash.sort.to_h
 end
 
-p word_sizes('Four score and seven.')
-p word_sizes('Hey diddle diddle, the cat and the fiddle!')
-p word_sizes("What's up doc?")
-p word_sizes('')
 p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
 p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
 p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
 p word_sizes('') == {}
 
+#___Launch school Solution________________________________
 
+def word_sizes(words_string)
+  counts = Hash.new(0)
+  words_string.split.each do |word|
+    counts[word.size] += 1
+  end
+  counts
+end
 
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+p word_sizes('') == {}
