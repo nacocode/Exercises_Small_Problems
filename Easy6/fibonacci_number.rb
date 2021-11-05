7a3ae6544bfb4bdcc28ae60ca6322d2ce50476c1
nacocode <naco120692@gmail.com>
2021-11-05 14:54:38 -0400

    The first attempt

---
+++ Easy6/fibonacci_number.rb
@@ -0,0 +1,28 @@
+# find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
+# find_fibonacci_index_by_length(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
+# find_fibonacci_index_by_length(10) == 45
+# find_fibonacci_index_by_length(100) == 476
+# find_fibonacci_index_by_length(1000) == 4782
+# find_fibonacci_index_by_length(10000) == 47847
+# 
+# argumentが何桁かを指名してる。
+# fibonacchiの計算をしてアウトプットしていって
+# アウトプットの桁がargumentの数字と同じになったらストップ
+# リターン値は最後にプリントした数字
+
+def find_fibonacci_index_by_length(num)
+  array = [1,1]
+  index = 0
+  until array[-1].to_s.length == num
+  array << array[index] + array[index + 1]
+  index += 1
+  end
+  array.size
+end
+
+p find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
+p find_fibonacci_index_by_length(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
+p find_fibonacci_index_by_length(10) == 45
+p find_fibonacci_index_by_length(100) == 476
+p find_fibonacci_index_by_length(1000) == 4782
+p find_fibonacci_index_by_length(10000) == 47847
