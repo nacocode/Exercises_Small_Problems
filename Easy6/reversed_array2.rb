7a83ec0f47fe0ca5198f69ced7f45d60466e3789
nacocode <naco120692@gmail.com>
2021-11-08 14:21:28 -0500

    my solution

---
+++ Easy6/reversed_array2.rb
@@ -0,0 +1,22 @@
+def reverse(arr)
+  reversed_arr = []
+  arr_index = -1
+  reversed_arr_index = 0
+  while reversed_arr_index < arr.size
+    reversed_arr[reversed_arr_index] = arr[arr_index]
+    reversed_arr_index += 1
+    arr_index -= 1
+  end
+  reversed_arr
+end
+
+p reverse([1,2,3,4]) == [4,3,2,1]          # => true
+p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+p reverse(['abc']) == ['abc']              # => true
+p reverse([]) == []                        # => true
+
+list = [1, 3, 2]                      # => [1, 3, 2]
+new_list = reverse(list)              # => [2, 3, 1]
+p list.object_id != new_list.object_id  # => true
+p list == [1, 3, 2]                     # => true
+p new_list == [2, 3, 1]                 # => true
