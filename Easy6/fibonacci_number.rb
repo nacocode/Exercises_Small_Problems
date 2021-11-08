306bfc88800c68ded87f302e6724f1647d2188e1
nacocode <naco120692@gmail.com>
2021-11-08 11:38:51 -0500

    Added the launch school's solution

--- Easy6/fibonacci_number.rb
+++ Easy6/fibonacci_number.rb
@@ -16,3 +16,22 @@ p find_fibonacci_index_by_length(10) == 45
 p find_fibonacci_index_by_length(100) == 476
 p find_fibonacci_index_by_length(1000) == 4782
 p find_fibonacci_index_by_length(10000) == 47847
+
+#____Launch School Solution________________________
+
+def find_fibonacci_index_by_length(number_digits)
+  first = 1
+  second = 1
+  index = 2
+
+  loop do
+    index += 1
+    fibonacci = first + second
+    break if fibonacci.to_s.size >= number_digits
+
+    first  = second
+    second = fibonacci
+  end
+
+  index
+end
