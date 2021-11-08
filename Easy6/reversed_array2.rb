cffb006f51a4ea3bdfe349ca04e61c10160ba369
nacocode <naco120692@gmail.com>
2021-11-08 14:22:58 -0500

    Added launch school solution

--- Easy6/reversed_array2.rb
+++ Easy6/reversed_array2.rb
@@ -20,3 +20,11 @@ new_list = reverse(list)              # => [2, 3, 1]
 p list.object_id != new_list.object_id  # => true
 p list == [1, 3, 2]                     # => true
 p new_list == [2, 3, 1]                 # => true
+
+#__Launch School Solution____________________________
+
+def reverse(array)
+  result_array = []
+  array.reverse_each { |element| result_array << element }
+  result_array
+end
