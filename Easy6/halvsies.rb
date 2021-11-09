bf5367b8c742dc3dc5f8813561ee5983d668446b
nacocode <naco120692@gmail.com>
2021-11-09 08:00:13 -0500

    first attempt

---
+++ Easy6/halvsies.rb
@@ -0,0 +1,15 @@
+def halvsies(array)
+  half = ((array.size.to_f / 2).round) - 1
+  first = array[0..half] 
+  second = array[(half + 1)..-1]
+  result = []
+  result << first
+  result << second
+  result
+end
+
+
+p halvsies([1, 2, 3, 4]) 
+p halvsies([1, 5, 2, 4, 3]) 
+p halvsies([5])
+p halvsies([])
