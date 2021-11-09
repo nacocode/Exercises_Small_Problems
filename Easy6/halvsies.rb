773788c50b59b32c0e2cde8c9828b3b2d1fb07dd
nacocode <naco120692@gmail.com>
2021-11-09 09:32:19 -0500

    Compaired with another solution

--- Easy6/halvsies.rb
+++ Easy6/halvsies.rb
@@ -1,12 +1,20 @@
 def halvsies(array)
-  half = ((array.size / 2.0).round) - 1
+  half = ((array.size / 2.0).ceil) -1
   first = array[0..half] 
   second = array[(half + 1)..-1]
   result = [first, second]
 end
 
-
 p halvsies([1, 2, 3, 4]) 
 p halvsies([1, 5, 2, 4, 3]) 
 p halvsies([5])
 p halvsies([])
+
+#_________Launch School Solution____________________
+
+def halvsies(array)
+  middle = (array.size / 2.0).ceil
+  first_half = array.slice(0, middle)
+  second_half = array.slice(middle, array.size - middle)
+  [first_half, second_half]
+end
