b2fb33faaa3bb512bb0f700db51712bb8eccfa80
nacocode <naco120692@gmail.com>
2021-11-10 09:39:16 -0500

    Added LS solution

--- Easy7/combine_2_lists.rb
+++ Easy7/combine_2_lists.rb
@@ -7,8 +7,18 @@ def interleave(arr1, arr2)
     index += 1
     break if index == arr1.size
   end
-
+  
   result
 end
+
+#______________LS Solution______________________
+
+def interleave(arr1, arr2)
+  result = []
+  arr1.each_with_index do |element, index|
+    result << element << arr2[index]
+  result
+end
+
 
 p interleave([1, 2, 3], ['a', 'b', 'c']) #== [1, 'a', 2, 'b', 3, 'c']
