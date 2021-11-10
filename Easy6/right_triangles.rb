d41675e1e8a3d1590e5de03e5a9a83a68e5eb1b5
nacocode <naco120692@gmail.com>
2021-11-09 22:07:10 -0500

    Modified an  output code

--- Easy6/right_triangles.rb
+++ Easy6/right_triangles.rb
@@ -1,9 +1,22 @@
 def triangle(num)
   count = 1
   num.times do
-    puts "#{" " * (num - count)}#{"*" * count}"
+    puts "#{" " * (num - count)}#{"*" * count}"       # or => puts (" " * (num - count)) + ("*" * count) also works
     count += 1
   end
 end
 
 triangle(5)
+
+#_______Launch School Solution__________________
+
+def triangle(num)
+  spaces = num - 1
+  stars = 1
+
+  num.times do |n|
+    puts (' ' * spaces) + ('*' * stars)
+    spaces -= 1
+    stars += 1
+  end
+end
