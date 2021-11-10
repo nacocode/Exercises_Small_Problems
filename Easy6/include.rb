256382ca98bd27b0e1b5d50120f3ec100c91a2b7
nacocode <naco120692@gmail.com>
2021-11-10 07:56:01 -0500

    Modified parameter's name and added LS solution

--- Easy6/include.rb
+++ Easy6/include.rb
@@ -1,5 +1,5 @@
-def include?(array, num)
-  array.any? {|element| element == num }
+def include?(array, value)
+  array.any? {|element| element == value }
 end
 
 p include?([1,2,3,4,5], 3)
@@ -7,3 +7,9 @@ p include?([1,2,3,4,5], 6)
 p include?([], 3)
 p include?([nil], nil)
 p include?([], nil)
+
+#_______Launch School Solution below________
+
+def include?(array, value)
+  !!array.find_index(value)
+end
