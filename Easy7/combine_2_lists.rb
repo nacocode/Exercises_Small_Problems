ee049901fc2ac0db9d64e7e24dfa601c099febbb
nacocode <naco120692@gmail.com>
2021-11-10 09:34:45 -0500

    Refactored the way to assign the elements to the result array

--- Easy7/combine_2_lists.rb
+++ Easy7/combine_2_lists.rb
@@ -1,16 +1,14 @@
 def interleave(arr1, arr2)
-  index = 0
   result = []
+  index = 0
 
   loop do
-    result << arr1[index]
-    result << arr2[index]
+    result << arr1[index] << arr2[index]
     index += 1
     break if index == arr1.size
   end
 
   result
 end
-
 
 p interleave([1, 2, 3], ['a', 'b', 'c']) #== [1, 'a', 2, 'b', 3, 'c']
