8b27b0351efb9ab27d08f51493409c11d0ef7d6f
nacocode <naco120692@gmail.com>
2021-11-17 12:09:41 -0500

    done

---
+++ Easy7/multiplicative_average.rb
@@ -0,0 +1,10 @@
+def show_multiplicative_average(numbers)
+  product = 1.to_f
+  numbers.each { |num| product *= num }
+  average = product / numbers.size
+  puts "The result is %.3f" % average
+end
+
+show_multiplicative_average([3, 5])
+show_multiplicative_average([6])
+show_multiplicative_average([2, 5, 7, 11, 13, 17])
