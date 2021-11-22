9cb5ac72bb42fe1b8bb5db0b8e17731c20716cfb
nacocode <naco120692@gmail.com>
2021-11-22 12:25:45 -0500

    :bug: moved the code to find a number which is divisible by 3 and 5 fist in the if statement.

--- Easy8/fizzbuzz.rb
+++ Easy8/fizzbuzz.rb
@@ -2,18 +2,40 @@ def fizzbuzz(num1, num2)
   array = (num1..num2).to_a
 
   result = array.map do |num|
-    if num % 3 == 0
-      num = "Fizz"
+    if num % 3 == 0 && num % 5 == 0
+      num = "FizzBuzz"
     elsif num % 5 == 0
       num = "Buzz"
-    elsif num % 3 == 0 && num % 5 == 0
-      num = "FizzBuzz"
+    elsif num % 3 == 0
+      num = "Fizz"
     else
       num
     end
   end
   result
 end
+
+#def fizzbuzz(num1, num2)
+#  result = []
+#  num1.upto(num2) do |num|
+#   result << fizzbuzz_value(num)
+#  end
+#
+#  result
+#end
+#
+#def fizzbuzz_value(num)
+#  case
+#  when num % 3 == 0 && num % 5 == 0
+#    "FizzBuzz"
+#  when num % 3 == 0
+#    "Fizz"
+#  when num % 5 == 0
+#    "Buzz"
+#  else
+#    num
+#  end
+#end
 
 p fizzbuzz(1, 15) 
 # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
