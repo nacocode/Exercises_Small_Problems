33b264ee4cfc7252e29a20a4f22df006288d451a
nacocode <naco120692@gmail.com>
2021-11-22 12:30:39 -0500

    :bug: Refactored the result output to the asked style .

--- Easy8/fizzbuzz.rb
+++ Easy8/fizzbuzz.rb
@@ -12,30 +12,8 @@ def fizzbuzz(num1, num2)
       num
     end
   end
-  result
+  result.join(", ")
 end
-
-#def fizzbuzz(num1, num2)
-#  result = []
-#  num1.upto(num2) do |num|
-#   result << fizzbuzz_value(num)
-#  end
-#
-#  result
-#end
-#
-#def fizzbuzz_value(num)
-#  case
-#  when num % 3 == 0 && num % 5 == 0
-#    "FizzBuzz"
-#  when num % 3 == 0
-#    "Fizz"
-#  when num % 5 == 0
-#    "Buzz"
-#  else
-#    num
-#  end
-#end
 
 p fizzbuzz(1, 15) 
 # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
