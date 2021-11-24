a5e32d80b3b90589ab63d1b14832100fed0bd457
nacocode <naco120692@gmail.com>
2021-11-24 11:58:37 -0500

    Refactor: used each_char method instead of regex.

--- Easy8/double_char2.rb
+++ Easy8/double_char2.rb
@@ -1,8 +1,10 @@
+CONSONANTS = %w(b c d f g h j k l m n p q r s t v w x y z)
+
 def double_consonants(string)
   result = ""
   string.each_char do |char|
-    if char.match?(/[^aeiou\W\d]/)
-    result << char << char
+    if CONSONANTS.include?(char.downcase)
+      result << char << char
     else
       result << char
     end
@@ -14,3 +16,14 @@ p double_consonants('String') == "SSttrrinngg"
 p double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
 p double_consonants("July 4th") == "JJullyy 4tthh"
 p double_consonants('') == ""
+
+#_______________LS Solution_______________________
+
+def double_consonants(string)
+  result = ''
+  string.each_char do |char|
+    result << char
+    result << char if CONSONANTS.include?(char.downcase)
+  end
+  result
+end
