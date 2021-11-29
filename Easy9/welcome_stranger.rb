435d7e928c8e778b8410ad4899b1ac8a2c29d19b
nacocode <naco120692@gmail.com>
2021-11-29 11:27:17 -0500

    :bug: refactoring with rubocop

--- Easy9/welcome_stranger.rb
+++ Easy9/welcome_stranger.rb
@@ -1,5 +1,5 @@
 def greetings(array, hash)
-  puts "Hello, #{array.join(" ")}! Nice to have a #{hash[:title]} #{hash[:occupation]} around."
+  puts "Hello, #{array.join(' ')}! Nice to have a #{hash[:title]} #{hash[:occupation]} around."
 end
 
 greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
