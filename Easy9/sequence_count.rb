5e2bc8dba0db121a0d8f2ac07a5313d32d3cf3f3
nacocode <naco120692@gmail.com>
2021-12-02 10:33:24 -0500

    added LS solution

--- Easy9/sequence_count.rb
+++ Easy9/sequence_count.rb
@@ -10,3 +10,17 @@ p sequence(5, 1) == [1, 2, 3, 4, 5]
 p sequence(4, -7) == [-7, -14, -21, -28]
 p sequence(3, 0) == [0, 0, 0]
 p sequence(0, 1000000) == []
+
+# ________LS Solution_________
+
+def sequence(count_num, first_num)
+  sequence = []
+  number = first_num
+
+  count_num.times do
+    sequence << number
+    number += first_num
+  end
+
+  sequence
+end
