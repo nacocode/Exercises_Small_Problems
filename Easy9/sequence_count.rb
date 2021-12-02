0d23ed25638332f05053f66a1add521048c44ed7
nacocode <naco120692@gmail.com>
2021-12-02 09:50:26 -0500

    My solution

---
+++ Easy9/sequence_count.rb
@@ -0,0 +1,12 @@
+def sequence(count_num, first_num)
+  sequence = []
+  count_num.times do |i|
+    sequence << (first_num * (i + 1))
+  end
+  sequence
+end
+
+p sequence(5, 1) == [1, 2, 3, 4, 5]
+p sequence(4, -7) == [-7, -14, -21, -28]
+p sequence(3, 0) == [0, 0, 0]
+p sequence(0, 1000000) == []
