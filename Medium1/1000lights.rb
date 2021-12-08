29a13b2c1a763b7985abb358c22a98b8d7a8dd10
nacocode <naco120692@gmail.com>
2021-12-08 16:53:30 -0500

    Refactor: Extracted 2 method and improved a readability.

--- Medium1/1000lights.rb
+++ Medium1/1000lights.rb
@@ -2,26 +2,32 @@ def multiple?(number, divisor)
   number % divisor == 0
 end
 
-def toggle_switch(number_of_switch)
-  switches = Hash.new
-  1.upto(number_of_switch) do |num|
-    switches[num] = false
-  end
+def initialize_lights(number_of_lights)
+  lights = Hash.new
+  1.upto(number_of_lights) { |num| lights[num] = false }
+  lights
+end
 
-  1.upto(number_of_switch) do |num|
-    switches.each do |k, _|
-      if multiple?(k, num)
-        switches[k] = !switches[k]
+def on_lights(lights)
+  lights.select { |_, v| v == true }.keys
+end
+
+def toggle_switch(number_of_lights)
+  lights = initialize_lights(number_of_lights)
+
+  1.upto(number_of_lights) do |iteration_number|
+    lights.each do |k, _|
+      if multiple?(k, iteration_number)
+        lights[k] = !lights[k]  # or => lights[k] = (v == false) ? "on" : "off"
       end
     end
   end
 
-  result = []
-  switches.each { |k, v| result << k if v == true }
-  result
+  on_lights(lights)
 end
 
 p toggle_switch(1)
 p toggle_switch(2)
 p toggle_switch(5)
 p toggle_switch(10)
+p toggle_switch(1000)
