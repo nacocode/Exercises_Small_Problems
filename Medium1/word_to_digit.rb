1ba0901621b956fd35a2e4206e8372019649d278
nacocode <naco120692@gmail.com>
2021-12-09 14:02:27 -0500

    Used regex to solve the problem

---
+++ Medium1/word_to_digit.rb
@@ -0,0 +1,10 @@
+NUMBERS = { "one" => "1", "two" => "2", "three" => "3", "four" => "4",
+            "five" => "5", "six" => "6", "seven" => "7", "eight" => "8", "nine" => "9" }
+
+def word_to_digit(string)
+  NUMBERS.each { |k, v| p string.gsub!(k, v) }
+  string
+end
+
+p word_to_digit('Please call me at five five five one two three four. Thanks.')
+# == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
