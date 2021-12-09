0a4ca371c0a7d84822382ea0ac232b185a1dd1dd
nacocode <naco120692@gmail.com>
2021-12-09 14:50:00 -0500

    :bug:

--- Medium1/word_to_digit.rb
+++ Medium1/word_to_digit.rb
@@ -1,5 +1,6 @@
 DIGIT_NUMBERS = { "one" => "1", "two" => "2", "three" => "3", "four" => "4",
-            "five" => "5", "six" => "6", "seven" => "7", "eight" => "8", "nine" => "9" }
+                  "five" => "5", "six" => "6", "seven" => "7", "eight" => "8",
+                  "nine" => "9" }
 
 def word_to_digit(string)
   DIGIT_NUMBERS.each { |k, v| string.gsub!(k, v) }
