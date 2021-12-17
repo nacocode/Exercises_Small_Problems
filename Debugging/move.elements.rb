7e7249f6c9004c2e2223f438b98f3f7663e09624
nacocode <naco120692@gmail.com>
2021-12-17 15:10:13 -0500

    shift method

---
+++ Debugging/move.elements.rb
@@ -0,0 +1,19 @@
+def move(n, from_array, to_array)
+  return if n == 0
+  to_array << from_array.shift
+  move(n - 1, from_array, to_array)
+end
+
+# def move(n, from_array, to_array)
+#   n.times do
+#     to_array << from_array.shift
+#   end
+# end
+
+todo = ['study', 'walk the dog', 'coffee with Tom']
+done = ['apply sunscreen', 'go to the beach']
+
+move(2, todo, done)
+
+p todo # should be: ['coffee with Tom']
+p done # should be: ['apply sunscreen', 'go to the beach', 'study', 'walk the dog']
