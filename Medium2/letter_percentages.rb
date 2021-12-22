d50c2dba8d2e1c772664c07b064dae84094cfb34
nacocode <naco120692@gmail.com>
2021-12-22 16:28:42 -0500

    calculated the recio of letter percentages

---
+++ Medium2/letter_percentages.rb
@@ -0,0 +1,24 @@
+def letter_percentages(string)
+  counts = {}
+  percentages = {}
+  characters = string.chars
+  length = string.size
+
+  counts[:lowercase] = characters.count { |char| char =~ /[a-z]/ }
+  counts[:uppercase] = characters.count { |char| char =~ /[A-Z]/ }
+  counts[:neither] = characters.count { |char| char =~ /[^A-Za-z]/ }
+
+  calculate(counts, percentages, length)
+
+  percentages
+end
+
+def calculate(counts, percentages, length)
+  percentages[:lowercase] = (counts[:lowercase] / length.to_f) * 100
+  percentages[:uppercase] = (counts[:uppercase] / length.to_f) * 100
+  percentages[:neither] = (counts[:neither] / length.to_f) * 100
+end
+
+p letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
+p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
+p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
